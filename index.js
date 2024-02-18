63f917cd81691fc05a60eb7a0f64fcff316c4fed
kamal <kk5738547@gmail.com>
2024-02-18 23:54:22 +0530

    firstcommit.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express =require('express');
 const mongoose=require('mongoose');
+const punycode = require('punycode');
 const app = express();
 const port= 4000;
 app.use(express.json());
-const db="mongodb://localhost:27017/students";
+const db="mongodb://127.0.0.1:27017/students";
 const studentRoute = require("./routes/studentRoute");
 app.use('/students', studentRoute);
 
@@ -32,7 +33,7 @@ const options = {
 
 //connect mongodb
 
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 .then(()=>{
     console.log("connected to db");
 app.listen(port,()=>{
